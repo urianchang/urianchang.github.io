8e3022f5bfbb96698a4d49a41f210f101f146ba7
Urian Chang <urianchang@gmail.com>
2017-05-30 01:06:42 -0700

    Retooled navigation bar to change based on screen width. Fixed smooth scrolling to top

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,14 @@
+//: Add/Remove "responsive" class to navbar
+function showMenu() {
+    var x = document.getElementById("nav");
+    // console.log(x.className);
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
 $(document).ready( function() {
     //: Auto resize div based on window size
     $(function(){
